4c11db6a31432174feb66d9c33df52c257e6b7c5
Peter Nagy <155898084+Skoteinos1@users.noreply.github.com>
2024-09-06 23:56:05 +0200

    Update decrypt_test.py

--- decrypt_test.py
+++ decrypt_test.py
@@ -4,3 +4,6 @@ pth = '/PATH/TO/YOUR/FOLDER/'  # Has to be defined because of debug bug
 def save_pickle(file, data1):
     if '.pkl' not in file:
         file += '.pkl'
+    pkl_file = open(pth + file, 'wb')
+    pickle.dump(data1, pkl_file)
+    pkl_file.close()

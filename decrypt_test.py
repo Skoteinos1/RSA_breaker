47267b41ed3d2f8354b9e4ccfbcdda7bf9b4cc36
Peter Nagy <155898084+Skoteinos1@users.noreply.github.com>
2024-09-16 21:40:17 -0400

    Update decrypt_test.py

--- decrypt_test.py
+++ decrypt_test.py
@@ -32,3 +32,6 @@ def enc_decr(f1, f11, f2, ms):
     if d_ms == ms and e_ms != ms:
         e_ms = [(x**f11)%f2 for x in ms]
         d_ms = [(x**f1)%f2 for x in e_ms]
+        if d_ms == ms and e_ms != ms:
+            valid_key = True
+    return valid_key
